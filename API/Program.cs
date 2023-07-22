ccf92aa58edfa76889751e58b3c77c502802871e
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-07-22 23:26:48 +0700

    22-Jul-2023 11:26PM

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using API.Data;
-
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +20,8 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
--- client/src/features/catalog/ProductDetail.tsx
+++ client/src/features/catalog/ProductDetail.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
+import agent from "../../app/api/agent";
 
 
 export default function ProductDetailPage(){
@@ -12,8 +13,8 @@ export default function ProductDetailPage(){
     const [loading, setLoading] = useState(true);
 
     useEffect(()=>{
-        axios.get(`http://localhost:5000/api/products/${id}`)
-            .then(response => setProduct(response.data))
+            id && agent.Catalog.details(parseInt(id))
+            .then(response => setProduct(response))
             .catch(error => console.log(error))
             .finally(() => setLoading(false));
     }, [id])
