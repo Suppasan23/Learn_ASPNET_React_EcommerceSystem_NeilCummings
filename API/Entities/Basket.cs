943efca87624db09d313bacda45d4aa04a581409
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-07-27 16:55:31 +0700

    27-Jul-2023 4:55PM

---
+++ API/Entities/Basket.cs
@@ -0,0 +1,31 @@
+namespace API.Entities
+{
+    public class Basket
+    {
+        public int Id {get; set;}
+        public string BuyerId {get; set;}
+        public List<BasketItem> Items {get; set;} = new();
+
+        public void AddItem(Product product, int quantity)
+        {
+            if(Items.All(item => item.ProductId != product.Id))
+            {
+                Items.Add(new BasketItem{Product = product, Quantity = quantity});
+            }
+
+            var existingItem = Items.FirstOrDefault(item => item.ProductId == product.Id);
+            if(existingItem != null) existingItem.Quantity += quantity;
+        }
+
+        public void RemoveItem(int productId, int quantity)
+        {
+            var itemToRemove = Items.FirstOrDefault(item => item.ProductId == productId);
+            if(itemToRemove == null) return;
+
+            itemToRemove.Quantity -= quantity;
+
+            if(itemToRemove.Quantity == 0) 
+            Items.Remove(itemToRemove);
+        }
+    }
+}
