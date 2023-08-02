f2f06ee66052227643fd3d5dada4a102f6121229
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-08-02 18:43:19 +0700

    finish 79

--- client/src/features/catalog/ProductDetail.tsx
+++ client/src/features/catalog/ProductDetail.tsx
@@ -1,26 +1,33 @@
-import { Divider, Grid, Table, TableBody, TableCell, TableRow, Typography } from "@mui/material";
+import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, TextField, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
 import agent from "../../app/api/agent";
 import NotFound from "../../app/errors/NotFound";
 import LoadingComponent from "../../app/layout/loadingComponent";
+import { useStoreContext } from "../../app/context/StoreContext";
+import { LoadingButton } from "@mui/lab";
 
 
 export default function ProductDetailPage(){
+    const {basket} = useStoreContext();
     const {id} = useParams<{id: string}>();
     const [product, setProduct] = useState<Product | null>(null);
     const [loading, setLoading] = useState(true);
+    const [quantity, setQuantity] = useState(0);
+    const [submitting, setSubmitting] = useState(false);
+    const item = basket?.items.find(i => i.productId === product?.id);
 
     useEffect(() => {
         if (id === undefined || id === "") {
           return;
         }
+        if (item) setQuantity(item.quantity);
         agent.Catalog.details(parseInt(id))
           .then(response => setProduct(response))
           .catch(error => console.log(error))
           .finally(() => setLoading(false));
-      }, [id]);
+      }, [id, item]);
       
     if(loading) return <LoadingComponent message='Loading product...'/>
 
@@ -37,30 +44,57 @@ export default function ProductDetailPage(){
                 <Typography variant='h3'>{product.name}</Typography>
                 <Divider sx={{mb: 2}} />
                 <Typography variant='h4' color='secondary'>{(product.price / 100).toFixed(2)}</Typography>
-                    <Table>
-                        <TableBody>
-                            <TableRow>
-                                <TableCell>Name</TableCell>
-                                <TableCell>{product.name}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>Description</TableCell>
-                                <TableCell>{product.description}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>Type</TableCell>
-                                <TableCell>{product.type}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>Brand</TableCell>
-                                <TableCell>{product.brand}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>Quantity in stock</TableCell>
-                                <TableCell>{product.quantityInStock}</TableCell>
-                            </TableRow>
-                        </TableBody>
-                    </Table>
+                    <TableContainer>   
+                        <Table>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell>Name</TableCell>
+                                    <TableCell>{product.name}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Description</TableCell>
+                                    <TableCell>{product.description}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Type</TableCell>
+                                    <TableCell>{product.type}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Brand</TableCell>
+                                    <TableCell>{product.brand}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Quantity in stock</TableCell>
+                                    <TableCell>{product.quantityInStock}</TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer> 
+
+                    <Grid container spacing={2}>
+                        <Grid item xs={6}>
+                            <TextField
+                                variant="outlined"
+                                type='number'
+                                label='Quantity in Cart'
+                                fullWidth
+                                value={quantity}
+                            />
+                        </Grid>
+
+                        <Grid item xs={6}>
+                            <LoadingButton
+                                sx={{height:'55px'}}
+                                color='primary'
+                                size='large'
+                                variant='contained'
+                                fullWidth
+                            >
+                                {item ? 'Update Quantity' : 'Add to Cart'}
+                            </LoadingButton>
+                        </Grid>
+                    </Grid>  
+
             </Grid>
         </Grid>
     )
